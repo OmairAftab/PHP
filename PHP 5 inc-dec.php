584b0336c4f0904ca24cca286fff770bdafa2ec2
0mairAftab <omairch035@gmail.com>
2025-11-13 17:13:13 +0500

    covered all cases of  increment and decrement

---
+++ PHP 5 inc-dec.php
@@ -0,0 +1,27 @@
+<?php
+
+$a=10;
+echo $a++;  //pehle 10 e print hoga lekin baad ain is ki value main aik add ho jae ga
+echo "<br/>";
+
+echo $a; // upar a main increment kiya tha to ab ye wo vali value show kre ga
+echo "<br/>";
+
+echo ++$a;  //pehle jo a main value store thi us main 1 plus ho ga and phir value print hogi
+echo "<br/>";
+
+
+
+// same for --:
+$b=20;
+
+echo $b--;
+echo "<br/>";
+
+echo $b;
+echo "<br/>";
+
+echo --$b;
+
+
+?>
